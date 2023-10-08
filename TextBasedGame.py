27841dbe9093db8ad550d483bfdfe2fc2fda8d73
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-10-08 14:12:15 -0400

    created multiple prints for intro (easier to read)

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,7 +1,7 @@
 #Game introuction, Asks for user to enter last name and laysout the plot + controls
 name = input("Enter your last  name:")
 print(f"""
-Welcome to Twin Peaks, Specail Agent {name}.
+Welcome to Twin Peaks, Special Agent {name}.
 Your mission is to locate Special Agent Dale Cooper and remove him from Twin Peaks.
 His exact where-abouts are unknown at this time, but we suggest you explore the following:
 
@@ -13,11 +13,19 @@ The Sheriffs Station
 The Owl Cave
 The Black Lodge
 
+""")
+input ("Press any ket to continue...")
+
+print("""
 We know Special Agent Dale Cooper has left behind items in each of these locations.
 It is required of you to collect these materials.
 Do not enter the Black Lodge unless necessary.
 Under no circumatances should you enter The Black Lodge wihtout these itmes.
+""")
 
+input("Press any key to view controls.")
+
+print("""
 ENTER: "North", "South", "East", or "West" to move beteween rooms
 ENTER: "Get Item" to pick up items.
 
